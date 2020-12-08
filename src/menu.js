3e50778fa9d5cfe1277638c85da0f966fae01cb1
Neeraj Surjaye <neerajsurjaye@gmail.com>
2020-12-08 11:24:38 +0530

    v2

--- src/menu.js
+++ src/menu.js
@@ -11,23 +11,27 @@ let menu = (() => {
     menu.id = "main-section-menu";
     head.id = "menu-head";
     list.id = "list";
+
+
+    for(var i = 0 ; i < 4 ; i++){
+        list_items[i] = document.createElement("li");
+        list_items[i].classList.add("list-item");
+        list_items[i].textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit.";
+        list.appendChild(list_items[i]);      
+    }
     
     let init = ()=>{
         content.appendChild(menu);
         menu.appendChild(head);
         menu.appendChild(list);
 
-        for(var i = 0 ; i < 4 ; i++){
-            list_items[i] = document.createElement("li");
-            list_items[i].classList.add("list-item");
-            list_items[i].textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit.";
-            list.appendChild(list_items[i]);      
-        }
+        
     }
 
     let remove = ()=>{
         //console.log(document.getElementById("main-section-menu"));
         if(document.getElementById("main-section-menu")){
+            menu.removeChild(list);
             content.removeChild(menu);
         }
         //content.removeChild(head);
